2732306befeb3145a66feb2c1a1f5b374411f70c
Anthony Muhoro <anthonymuhoro2003@gmail.com>
2025-02-22 13:20:47 +0300

    manager update

--- src/routes/manager.routes.js
+++ src/routes/manager.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const { validate } = require('../middlewares/validate.middleware');
-const { registerSchema, loginSchema, changePasswordSchema, forgotPasswordSchema, verifyOtpSchema } = require('../validators/auth.validator');
+// const { validate } = require('../middlewares/validate.middleware');
+// const { registerSchema, loginSchema, changePasswordSchema, forgotPasswordSchema, verifyOtpSchema } = require('../validators/auth.validator');
 const managerAuthController = require('../controllers/manager.auth.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
 // Manager auth routes
-router.post('/signup', validate(registerSchema), managerAuthController.register);
-router.post('/login', validate(loginSchema), managerAuthController.login);
-router.post('/forgot-password', validate(forgotPasswordSchema), managerAuthController.forgotPassword);
-router.post('/change-password', authenticate, validate(changePasswordSchema), managerAuthController.changePassword);
-router.post('/verify-otp', validate(verifyOtpSchema), managerAuthController.verifyOTP);
-router.post('/resend-otp', validate(forgotPasswordSchema), managerAuthController.resendOTP);
+router.post('/signup',  managerAuthController.register);
+router.post('/login',  managerAuthController.login);
+router.post('/forgot-password', managerAuthController.forgotPassword);
+router.post('/change-password', authenticate,  managerAuthController.changePassword);
+router.post('/verify-otp',  managerAuthController.verifyOTP);
+router.post('/resend-otp', managerAuthController.resendOTP);
 
 module.exports = router;
